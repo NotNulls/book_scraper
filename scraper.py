7cf270e9e21167cf8ed2cec54044e0f5ac8e5802
Dusan Rangelov <dusanrangelov@yahoo.com>
2022-12-02 09:05:36 +0100

    Error cathing. Cleaning code. Pandas to CSV functionality.

--- scraper.py
+++ scraper.py
@@ -1,80 +1,87 @@
-import requests
 from bs4 import BeautifulSoup
-from pprint import pprint
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
+from selenium.common.exceptions import ElementClickInterceptedException, StaleElementReferenceException, TimeoutException, WebDriverException, InvalidSessionIdException
 import pandas as pd
 
-title_search = input('Please insert search keyword: ')
 
-def search_funct():
+
+
+def search_a_book():
+
+    title_search = input('Please insert search keyword: ')
+    print('Seeking results for: ', title_search)
 
     driver = webdriver.Firefox()
     driver.get('https://www.delfi.rs/')
-    time.sleep(3)
+    driver.maximize_window()
     driver.find_element(By.ID,'autocomplete-input').send_keys(title_search)
 
+    wait = WebDriverWait(driver, 10)
 
     action =ActionChains(driver)
-    time.sleep(1)
     action.key_down(Keys.ENTER).key_up(Keys.ENTER).perform()
 
+    cookie_catch =wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@id="uslovi"]')))
+    cookie_catch.click()
 
-    wait = WebDriverWait(driver,1)
-    time.sleep(2)
-    wait.until(EC.url_changes('https://www.delfi.rs/'))
+    search_result = driver.find_elements(By.CSS_SELECTOR, '.title-full-width h1')
+    s = [' '.join(s.text.split()) for s in search_result]
 
-    driver.find_element(By.CLASS_NAME,'modal-title')
+    list_of_pages = driver.find_elements(By.CLASS_NAME, "page-link")
 
-    soup = BeautifulSoup(driver.page_source, "html.parser")
-
-
-
-    search_result = soup.find_all('h1')
-    s = [' '.join(s.getText().split()) for s in search_result]
-    print('Web app search results: ',s)
-    print('----------')
-    print('Number of mathces: ', s[1][0])
 
     data = []
-    
-    if int(s[1][0]) != 0:
-        try:
-            soup = BeautifulSoup(driver.page_source,'html.parser')
-            find_by_class = soup.find_all(class_="body")
-            #print (find_by_class)
-            print('------type(i) and type(i.find_all("a",class_=""))----')
-            
-            for i in find_by_class:
-                title = ["".join(i.text) for i in i.find_all("a",class_="")][0]
-                author = [i.text for i in i.find_all("a",class_="")][1]
-                price = [i.text.split()[0] for i in i.find_all("span",class_="price")]
-                
-                
-                data.append([title,author, price[1]])
-            print('-------------------------')
-            # e = [[" ".join(item.getText().split()),] for item in 
-        except ValueError:
-            pass
-    else:
-        print('Please type in a correct keyword!')
+
+    #Check what the first result digit is. If > 0 means it has result hits. Proceeds further...
+    #print (s[0][0])
+
+    no_search_results = s[0][0]
+
+    if int(no_search_results) != 0:
         
-    
-    #print(data)
+        for _ in range(len(list_of_pages[1:-1])):    
+            soup = BeautifulSoup(driver.page_source, "html.parser")
+            find_by_class = soup.find_all(class_="body")
+                
+            for i in find_by_class:
+                
+                title = ["".join(i.text) for i in i.find_all("a",class_="")][0]
+                author = ', '.join([(i.text) for i in i.find_all("a",class_="")][1:])
+                
+                try:
+                    price = [i.text.split() for i in i.find_all("span",class_="usteda1")][0][3]
+                except (IndexError):
+                    price = [i.text.split() for i in i.find_all("span",class_="price")][0][3]
+                driver.implicitly_wait(2)
+                
 
-    try:
-        data_frame = pd.DataFrame(data)
-        data_frame.columns = ['Title','Author','Price']
-        print(data_frame)
-    except ValueError:
-        pass
+                data.append([title,author, price])
+            try:
+                driver.maximize_window()
+                next_page = driver.find_element(By.CSS_SELECTOR,'.pagination .page-item:last-child .page-link')
+                next_page.click()
+            except (StaleElementReferenceException, ElementClickInterceptedException,TimeoutException, WebDriverException, InvalidSessionIdException):
+                pass
+        
+        print('.............................')
+    else:
+        print('Please enter a relevant keyword!')
+        driver.quit()        
+        search_a_book()
 
-    driver.close()
+    data_frame = pd.DataFrame(data)
+    data_frame.columns = ['Title','Author','Price']
+    print(data_frame)
+    csv = data_frame.to_csv("csv_results_delfi_book_scraper",index_label="No.", na_rep=None)
+    #print(delfi_book_scraper)
 
-search_funct()
+    driver.quit()
+
+search_a_book()
+
 
